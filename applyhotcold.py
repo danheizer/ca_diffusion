3b393fe07269ba72a816030d672f9a377d5aa824
Dan Heizer <danheizer@live.com>
2015-04-27 21:52:37 -0700

    Apply hot cold
    Version 0.001

---
+++ applyhotcold.py
@@ -0,0 +1,16 @@
+"""
+bar = 2d array
+hotSites = list of Coordinates of hot
+coldSites = list of coordinates of cold
+"""
+def applyHotCold(bar, hotSites, coldSites):
+    newBar = bar
+    count = 0
+    while count < len(hotSites):
+        newBar[hotSites[0]][hotSites[1]] = HOT
+        count += 1
+    count = 0
+    while count < len(coldSites):
+        newBar[coldSites[0]][coldSites[1]] = COLD
+        count += 1
+    return newBar
